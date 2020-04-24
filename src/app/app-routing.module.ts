eab76fabc66768f910371acf56ab50bdf9a257c1
JeremyConn27 <jconn27@gmail.com>
2020-04-24 17:32:10 -0400

    initial layouts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PlatformComponent } from './platform/platform.component';
+import { EnterpriseComponent } from './enterprise/enterprise.component';
+import { SolutionsComponent } from './solutions/solutions.component';
+import { HelpComponent } from './help/help.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  {path: 'home', component: HomeComponent},
+  {path: 'platform', component: PlatformComponent},
+  {path: 'enterprise', component: EnterpriseComponent},
+  {path: 'solutions', component: SolutionsComponent},
+  {path: 'help', component: HelpComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
