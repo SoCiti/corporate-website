e2d1fbbe251054c4232c8fb5c08312ee62e82df1
JeremyConn27 <jconn27@gmail.com>
2020-05-17 20:19:07 -0400

    updated demo video

--- src/app/sneak-peek/sneak-peek.component.ts
+++ src/app/sneak-peek/sneak-peek.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SneakPeekComponent implements OnInit {
 
+  sideNav;
   constructor() { }
 
   ngOnInit(): void {
+    this.sideNav = document.querySelector('header');
+    this.sideNav.classList.add('stuck');
   }
 
 }
