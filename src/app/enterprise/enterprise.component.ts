1160546db9828b32dedc22f5d6298a27eef9e620
JeremyConn27 <jconn27@gmail.com>
2020-06-01 01:25:15 -0400

    updated enterprise

--- src/app/enterprise/enterprise.component.ts
+++ src/app/enterprise/enterprise.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-enterprise',
   templateUrl: './enterprise.component.html',
   styleUrls: ['./enterprise.component.less']
 })
 export class EnterpriseComponent implements OnInit {
+  activeSolution;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.activeSolution = 'education';
   }
 
+  viewEducation() {
+    this.activeSolution = 'education';
+  }
+
+  viewGov() {
+    this.activeSolution = 'gov';
+  }
+
+  viewNonProfit() {
+    this.activeSolution = 'nonProfit';
+  }
+
+  viewUnion() {
+    this.activeSolution = 'union';
+  }
 }
