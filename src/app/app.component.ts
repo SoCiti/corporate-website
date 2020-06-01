593dfdf3b088a7bcbfa67b6b10966a4d17cb30db
JeremyConn27 <jconn27@gmail.com>
2020-05-31 23:14:37 -0400

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component, ViewEncapsulation} from '@angular/core';
+import{Router, NavigationEnd} from '@angular/router';
 
+declare let gtag: Function;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
   encapsulation: ViewEncapsulation.None,
 })
+
+
 export class AppComponent {
   title = 'soCiti';
+  constructor(public router: Router){
+    this.router.events.subscribe(event => {
+       if(event instanceof NavigationEnd){
+           gtag('config', 'UA-167507804-1', 
+                 {
+                   'page_path': event.urlAfterRedirects
+                 }
+                );
+        }
+     }
+  )}
+
 
   onActivate(event) {
     window.scroll(0, 0);
--- src/assets/js/analytics.js
+++ src/assets/js/analytics.js
@@ -1,6 +1,4 @@
 
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-167507804-1"></script>
-
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
