6015d06992bd8f2edfdb20a7d3ecf4b50db40885
JeremyConn27 <jconn27@gmail.com>
2020-05-31 22:25:29 -0400

    updated analytics

--- src/assets/js/analytics.js
+++ src/assets/js/analytics.js
@@ -1,9 +1,8 @@
 
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-167507804-1"></script>
-<script>
+
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'UA-167507804-1');
-</script>
